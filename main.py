1a450bb5ce68a6536067d0cbac0b13f14b0ae7b9
Himalaya Pal <himalayapal123@gmail.com>
2021-07-11 17:43:01 +0545

    fixeed bug of authentication

--- main.py
+++ main.py
@@ -214,7 +214,7 @@ def add_new_post():
         db.session.add(new_post)
         db.session.commit()
         return redirect(url_for("get_all_posts"))
-    return render_template("make-post.html", form=form)
+    return render_template("make-post.html", form=form, logged_in=current_user.is_authenticated)
 
 
 @app.route("/edit-post/<int:post_id>", methods=["GET", "POST"])
